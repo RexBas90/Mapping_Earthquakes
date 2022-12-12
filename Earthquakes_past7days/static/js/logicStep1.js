4edda6d82c7f9edbc6c5bf6af96eea8235360015
Rekha <rekha.basra1@gmail.com>
2022-12-11 19:37:34 -0600

    Uploaded earthquake data from last 7 days

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -18,14 +18,14 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Create a base layer that holds both maps.
 let baseMaps = {
   "Streets": streets,
-  "Satellite Streets": satelliteStreets
+  "Satellite": satelliteStreets
 };
 
 // Create the map object with a center and zoom level.
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
+  center: [39.5, -98.5],
+  zoom: 3,
   layers: [streets]
 })
 
@@ -33,24 +33,26 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Add GeoJSON data.
-// Accessing the Toronto airline routes GeoJSON URL.
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/RexBas90/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Mapping_GeoJSON_Polygons/static/js/torontoNeighborhoods.json";
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
 
 // Create a style for the lines.
-let myStyle = {
-  color: "blue",
-  fillColor: "yellow",
-  opacity: 0.5,
-  weight: 2
-}
+//let myStyle = {
+  //color: "blue",
+  //fillColor: "yellow",
+  //opacity: 0.5,
+  //weight: 2
+//}
 
 
-d3.json(torontoHoods).then(function(data){
-  L.geoJson(data,{
-    style: myStyle,
-   onEachFeature: function(features, layer){
-      layer.bindPopup("<h3>" + features.properties.AREA_NAME + "</h3>")
-    }
-  }).addTo(map);
-  });
+//d3.json(torontoHoods).then(function(data){
+  //L.geoJson(data,{
+    //style: myStyle,
+   //onEachFeature: function(features, layer){
+    //  layer.bindPopup("<h3>" + features.properties.AREA_NAME + "</h3>")
+    //}
+  //}).addTo(map);
+  //});
